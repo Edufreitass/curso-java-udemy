50f1e0200b74d9ad088fb1679791739256a818fb
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-08-12 10:58:26 -0300

    Seção 14 - Aula 123: Herança

---
+++ src/com/udemy/java/nelioalves/secao14/aula123/application/Program.java
@@ -0,0 +1,15 @@
+package com.udemy.java.nelioalves.secao14.aula123.application;
+
+import com.udemy.java.nelioalves.secao14.aula123.entities.BusinessAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		BusinessAccount account = new BusinessAccount();
+
+		account.getBalance();
+
+	}
+
+}
