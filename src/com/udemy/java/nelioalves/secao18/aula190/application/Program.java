33e93864eb073225a7ef052dadb066115665555b
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-08-17 13:26:58 -0300

    Seção 18 - Aula 190: Solução do problema - PARTE 1

---
+++ src/com/udemy/java/nelioalves/secao18/aula190/application/Program.java
@@ -0,0 +1,11 @@
+package com.udemy.java.nelioalves.secao18.aula190.application;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
---
+++ src/com/udemy/java/nelioalves/secao18/aula190/model/services/RentalService.java
@@ -0,0 +1,22 @@
+package com.udemy.java.nelioalves.secao18.aula190.model.services;
+
+import com.udemy.java.nelioalves.secao18.aula190.model.entities.CarRental;
+
+public class RentalService {
+
+	private Double pricePerDay;
+	private Double pricePerHour;
+
+	private BrazilTaxService taxService;
+
+	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+
+	public void processInvoice(CarRental carRental) {
+
+	}
+
+}
