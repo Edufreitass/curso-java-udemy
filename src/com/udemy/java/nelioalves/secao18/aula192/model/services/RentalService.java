db951272fe5b96f70ecd53042b187a459f5f6c94
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-08-17 14:13:23 -0300

    Seção 18 - Aula 192: Solução do problema - PARTE 3 (com interface)

---
+++ src/com/udemy/java/nelioalves/secao18/aula192/model/services/RentalService.java
@@ -0,0 +1,38 @@
+package com.udemy.java.nelioalves.secao18.aula192.model.services;
+
+import com.udemy.java.nelioalves.secao18.aula192.model.entities.CarRental;
+import com.udemy.java.nelioalves.secao18.aula192.model.entities.Invoice;
+
+public class RentalService {
+
+	private Double pricePerDay;
+	private Double pricePerHour;
+
+	private TaxService taxService;
+
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		// (t2 - t1) = dif em ms, 1000=seg , 60=min , 60=horas
+		double hours = (double) (t2 - t1) / 1000 / 60 / 60;
+
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		} else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+
+		double tax = taxService.tax(basicPayment);
+
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+
+	}
+
+}
---
+++ src/com/udemy/java/nelioalves/secao18/aula192/model/services/TaxService.java
@@ -0,0 +1,7 @@
+package com.udemy.java.nelioalves.secao18.aula192.model.services;
+
+public interface TaxService {
+
+	double tax(double amount);
+
+}
