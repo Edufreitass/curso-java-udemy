dca17c3bf4b49f9a42f7c81418dd97f15b9776ef
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-08-18 14:49:50 -0300

    Seção 19 - Aula 209: Como TreeSet compara os elementos

---
+++ src/com/udemy/java/nelioalves/secao19/aula209/application/Program.java
@@ -0,0 +1,24 @@
+package com.udemy.java.nelioalves.secao19.aula209.application;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import com.udemy.java.nelioalves.secao19.aula209.entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Set<Product> set = new TreeSet<>();
+
+		set.add(new Product("TV", 900.0));
+		set.add(new Product("Notebook", 1200.0));
+		set.add(new Product("Tablet", 400.0));
+
+		for (Product p : set) {
+			System.out.println(p);
+		}
+
+	}
+
+}
