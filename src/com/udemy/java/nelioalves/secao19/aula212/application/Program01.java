5a35723e3182cbc1d4ac6b9781649356cebf4c82
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-08-18 21:03:49 -0300

    Seção 19 - Aula 212: Map

---
+++ src/com/udemy/java/nelioalves/secao19/aula212/application/Program01.java
@@ -0,0 +1,32 @@
+package com.udemy.java.nelioalves.secao19.aula212.application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+
+		Map<String, String> cookies = new TreeMap<>();
+
+		// K = key, V = Value
+		cookies.put("username", "Maria");
+		cookies.put("email", "maria@gmail.com");
+		cookies.put("phone", "99811122");
+
+		cookies.remove("email");
+		cookies.put("phone", "99771133");
+
+		System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+		System.out.println("Phone number: " + cookies.get("phone"));
+		System.out.println("Email: " + cookies.get("email"));
+		System.out.println("Size: " + cookies.size());
+
+		System.out.println("ALL COOKIES:");
+		for (String key : cookies.keySet()) {
+			System.out.println(key + " : " + cookies.get(key));
+		}
+
+	}
+
+}
