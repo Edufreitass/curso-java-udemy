d8bf0061bcf3b96db291fe32eef379e2a1908b3d
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-08-19 15:23:18 -0300

    Seção 20 - Aula 225: Exercício resolvido (stream)

---
+++ src/com/udemy/java/nelioalves/secao20/aula225/exercicio/resolvido/application/Program.java
@@ -0,0 +1,65 @@
+package com.udemy.java.nelioalves.secao20.aula225.exercicio.resolvido.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import com.udemy.java.nelioalves.secao20.aula225.exercicio.resolvido.entities.Product;
+
+public class Program {
+
+	/*
+	 * Fazer um programa para ler um conjunto de produtos a partir de um arquivo em
+	 * formato .csv (suponha que exista pelo menos um produto). Em seguida mostrar o
+	 * preço médio dos produtos. Depois, mostrar os nomes, em ordem decrescente, dos
+	 * produtos que possuem preço inferior ao preço médio. Veja exemplo na próxima
+	 * página.
+	 */
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		// C:\Users\Eduardo\Desktop\in.txt
+		System.out.print("Enter full file path: ");
+		String path = scan.nextLine();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			List<Product> list = new ArrayList<>();
+
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+				line = br.readLine();
+			}
+
+			// Usando pipeline para achar a soma dos preços e também a média
+			double avg = list.stream()
+					.map(p -> p.getPrice())
+					.reduce(0.0, (x, y) -> x + y) / list.size();
+			
+			System.out.println("Average price: " + String.format("%.2f", avg));
+			
+			Comparator<String> comparator = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+			List<String> names = list.stream()
+					.filter(p -> p.getPrice() < avg)
+					.map(p -> p.getName())
+					.sorted(comparator.reversed())
+					.collect(Collectors.toList());
+			
+			names.forEach(System.out::println);
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		scan.close();
+	}
+
+}
